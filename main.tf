7103fbd571968d49972eea611c3bac8dd1b29e2d
vish3080 <143146385+vish3080@users.noreply.github.com>
2024-11-24 22:58:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_lb" "myalb" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "myTG"
+  name     = "my"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.myvpc.id
